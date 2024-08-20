modified fov_2_average_gap.py. Still giving wrong results

data_analysis.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import matplotlib.mlab as mlab
 from scipy.optimize import curve_fit
-from pypcd4 import PointCloud
+#from pypcd4 import PointCloud
 
 def load_point_clouds(directory):
     ''' It gets only pcd points from a specific time frame '''
@@ -198,7 +198,6 @@ def plot_point_cloud_with_plane(ply_file, a, b, c, d):
 
     #plt.show()
     
-
 # Define the Gaussian function
 def gaussian(x, mu, sigma):
     return norm.pdf(x, mu, sigma)
@@ -360,33 +359,33 @@ Fuse diferent frames
 # -------------------- FIND PLANE and PLOT Noise distribution ------------------------------
 
 ############## Example for a whole folder:
-ply_files = [f for f in os.listdir(ply_directory)] 
-print(ply_files)
-for ply_file in ply_files:
-    ply_file_name = os.path.join(ply_directory,ply_file)
-    ply = o3d.io.read_point_cloud(ply_file_name)
-    # ply_file_name = "ply/P23_71.9_vert.ply"
-    # ply = o3d.io.read_point_cloud("ply/P23_71.9_vert.ply")
-    print(ply)
+# ply_files = [f for f in os.listdir(ply_directory)] 
+# print(ply_files)
+# for ply_file in ply_files:
+# ply_file_name = os.path.join(ply_directory,ply_file)
+# ply = o3d.io.read_point_cloud(ply_file_name)
+# # ply_file_name = "ply/P23_71.9_vert.ply"
+# # ply = o3d.io.read_point_cloud("ply/P23_71.9_vert.ply")
+# print(ply)
 
-    # Segment the plane TODO - change name of graph
-    plane_model, inliers = ply.segment_plane(distance_threshold=0.02, ransac_n=3, num_iterations=100) # 0.02 = 2 cm
-    [a, b, c, d] = plane_model
-    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-    plot_point_cloud_with_plane(ply_file_name, a, b, c, d)
-    calculate_gaussian_noise(ply_file_name, a, b, c, d)  # Threshold is 1 cm
-
-# ############## Example for a single file:
-# ply_path = "data/filtered_data/ply/72.7parking_contre_solei_blanche_74.7_2024-08-02-07-53-28.ply"
-# ply_directory = os.path.dirname(ply_path)
-# file_name, file_extension = os.path.splitext(os.path.basename(ply_path))
-
-# ply = o3d.io.read_point_cloud(ply_path)
-# # Threshold at 2 meters so it wont filter any points, as the planes are already croped
-# plane_model, inliers = ply.segment_plane(distance_threshold=2, ransac_n=3, num_iterations=100) 
+# # Segment the plane TODO - change name of graph
+# plane_model, inliers = ply.segment_plane(distance_threshold=0.02, ransac_n=3, num_iterations=100) # 0.02 = 2 cm
 # [a, b, c, d] = plane_model
-# plot_point_cloud_with_plane(ply_path, a, b, c, d)
-# calculate_gaussian_noise(ply_path, a, b, c, d)  # Threshold is 1 cm
+# print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+# plot_point_cloud_with_plane(ply_file_name, a, b, c, d)
+# calculate_gaussian_noise(ply_file_name, a, b, c, d)  # Threshold is 1 cm
+
+############### Example for a single file:
+ply_path = '/home/ws/src/data_lidar/filtered_data/2AUG_parking/ply/hand_vert_71.6_2024-08-02-08-01-45.ply'
+ply_directory = os.path.dirname(ply_path)
+file_name, file_extension = os.path.splitext(os.path.basename(ply_path))
+
+ply = o3d.io.read_point_cloud(ply_path)
+# Threshold at 2 meters so it wont filter any points, as the planes are already croped
+plane_model, inliers = ply.segment_plane(distance_threshold=2, ransac_n=3, num_iterations=100) 
+[a, b, c, d] = plane_model
+plot_point_cloud_with_plane(ply_path, a, b, c, d)
+calculate_gaussian_noise(ply_path, a, b, c, d)  # Threshold is 1 cm
 
 #ref next approach
 #ref como talvez usa pose graph https://www.open3d.org/docs/latest/tutorial/Advanced/multiway_registration.html

fov_heatmap.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-def process_data(file_path, nscans, threshold, show_plots):
+def calculate_fov(file_path, nscans, threshold, show_plots):
     # Read the CSV file
     scan_points_total = 2160 * nscans
     df = pd.read_csv(file_path, delimiter=',', nrows=scan_points_total)
@@ -60,12 +60,14 @@ def process_data(file_path, nscans, threshold, show_plots):
         
         # Plot the FOV coverage
         plt.figure()
-        plt.plot(range(1, len(fov_per) + 1), fov_per)
+        plt.plot(np.linspace(1, nscans/10,len(fov_per)), fov_per)
         plt.title(f'FOV coverage after {len(fov_per)/10} seconds')
-        plt.xlabel('Scan Index')
+        plt.xlabel('Seconds ')
         plt.ylabel('Coverage [%]')
         plt.grid()
+        plt.ylim(0, 100)
         plt.show()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process scan data and plot results.')
@@ -79,4 +81,4 @@ if __name__ == '__main__':
     # Determine if plotting should be shown
     show_plots = not args.no_plot
 
-    process_data(args.file_path, args.nscans, args.threshold, show_plots)
+    calculate_fov(args.file_path, args.nscans, args.threshold, show_plots)

imu.py
@@ -33,7 +33,7 @@ def main(args):
     """""""""""""""""""""""""""""
     " IMU data  Directory Path  "
     """""""""""""""""""""""""""""
-    extractDataPath = "/home/ws/src/point_lio_ws/data_filtering/data/IMU" # expanduser("~") + '/imu_data_raw/'
+    extractDataPath = "/home/ws/src/data_lidar/filtered_data/2AUG_parking/imu" # expanduser("~") + '/imu_data_raw/'
     if not os.path.isdir(extractDataPath):
         os.mkdir(extractDataPath)
 
@@ -110,36 +110,39 @@ def main(args):
     plt.figure(figsize=(12, 8))
 
     # Plot each sensor's data against time
-    plt.subplot(3, 1, 1)
+    plt.subplot(2, 1, 1)
     plt.plot(time, ax, label='Ax', color='r')
     plt.plot(time, ay, label='Ay', color='g')
     plt.plot(time, az, label='Az', color='b')
-    plt.xlabel('Time')
-    plt.ylabel('Acceleration')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Acceleration [m/s2]')
     plt.title('Acceleration vs Time')
     plt.legend()
+    plt.ylim(-5, +5)
 
-    plt.subplot(3, 1, 2)
+    plt.subplot(2, 1, 2)
     plt.plot(time, gx, label='Gx', color='r')
     plt.plot(time, gy, label='Gy', color='g')
     plt.plot(time, gz, label='Gz', color='b')
-    plt.xlabel('Time')
-    plt.ylabel('Angular Velocity')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Angular Velocity [degrees/s***]')
     plt.title('Angular Velocity vs Time')
     plt.legend()
+    plt.ylim(-0.10, +0.10)
 
-    plt.subplot(3, 1, 3)
-    plt.plot(time, ax, label='Ax', color='r', alpha=0.5)
-    plt.plot(time, ay, label='Ay', color='g', alpha=0.5)
-    plt.plot(time, az, label='Az', color='b', alpha=0.5)
-    plt.plot(time, gx, label='Gx', color='r', linestyle='--')
-    plt.plot(time, gy, label='Gy', color='g', linestyle='--')
-    plt.plot(time, gz, label='Gz', color='b', linestyle='--')
-    plt.grid()
-    plt.xlabel('Time')
-    plt.ylabel('Combined Data')
-    plt.title('Combined Plot')
-    plt.legend()
+
+    # plt.subplot(3, 1, 3)
+    # plt.plot(time, ax, label='Ax', color='r', alpha=0.5)
+    # plt.plot(time, ay, label='Ay', color='g', alpha=0.5)
+    # plt.plot(time, az, label='Az', color='b', alpha=0.5)
+    # plt.plot(time, gx, label='Gx', color='r', linestyle='--')
+    # plt.plot(time, gy, label='Gy', color='g', linestyle='--')
+    # plt.plot(time, gz, label='Gz', color='b', linestyle='--')
+    # plt.grid()
+    # plt.xlabel('Time')
+    # plt.ylabel('Combined Data')
+    # plt.title('Combined Plot')
+    # plt.legend()
 
     # Adjust layout
     plt.tight_layout()
@@ -169,7 +172,7 @@ def main(args):
     plt.figure(figsize=(12, 8))
 
     # Plot Fourier Transforms
-    plt.subplot(3, 1, 1)
+    plt.subplot(2, 1, 1)
     plt.plot(xf, 2.0/N * np.abs(yf_ax[:N//2]), label='Ax', color='r')
     plt.plot(xf, 2.0/N * np.abs(yf_ay[:N//2]), label='Ay', color='g')
     plt.plot(xf, 2.0/N * np.abs(yf_az[:N//2]), label='Az', color='b')
@@ -177,8 +180,10 @@ def main(args):
     plt.ylabel('Amplitude')
     plt.title('Acceleration Frequency Spectrum')
     plt.legend()
+    # plt.xlim(1, 4)
+    # plt.ylim(1, 16)
 
-    plt.subplot(3, 1, 2)
+    plt.subplot(2, 1, 2)
     plt.plot(xf, 2.0/N * np.abs(yf_gx[:N//2]), label='Gx', color='r')
     plt.plot(xf, 2.0/N * np.abs(yf_gy[:N//2]), label='Gy', color='g')
     plt.plot(xf, 2.0/N * np.abs(yf_gz[:N//2]), label='Gz', color='b')
@@ -186,18 +191,20 @@ def main(args):
     plt.ylabel('Amplitude')
     plt.title('Angular Velocity Frequency Spectrum')
     plt.legend()
+    # plt.xlim(1, 4)
+    # plt.ylim(1, 16)
 
-    plt.subplot(3, 1, 3)
-    plt.plot(xf, 2.0/N * np.abs(yf_ax[:N//2]), label='Ax', color='r', alpha=0.5)
-    plt.plot(xf, 2.0/N * np.abs(yf_ay[:N//2]), label='Ay', color='g', alpha=0.5)
-    plt.plot(xf, 2.0/N * np.abs(yf_az[:N//2]), label='Az', color='b', alpha=0.5)
-    plt.plot(xf, 2.0/N * np.abs(yf_gx[:N//2]), label='Gx', color='r', linestyle='--')
-    plt.plot(xf, 2.0/N * np.abs(yf_gy[:N//2]), label='Gy', color='g', linestyle='--')
-    plt.plot(xf, 2.0/N * np.abs(yf_gz[:N//2]), label='Gz', color='b', linestyle='--')
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Combined Amplitude')
-    plt.title('Combined Frequency Spectrum')
-    plt.legend()
+    # plt.subplot(3, 1, 3)
+    # plt.plot(xf, 2.0/N * np.abs(yf_ax[:N//2]), label='Ax', color='r', alpha=0.5)
+    # plt.plot(xf, 2.0/N * np.abs(yf_ay[:N//2]), label='Ay', color='g', alpha=0.5)
+    # plt.plot(xf, 2.0/N * np.abs(yf_az[:N//2]), label='Az', color='b', alpha=0.5)
+    # plt.plot(xf, 2.0/N * np.abs(yf_gx[:N//2]), label='Gx', color='r', linestyle='--')
+    # plt.plot(xf, 2.0/N * np.abs(yf_gy[:N//2]), label='Gy', color='g', linestyle='--')
+    # plt.plot(xf, 2.0/N * np.abs(yf_gz[:N//2]), label='Gz', color='b', linestyle='--')
+    # plt.xlabel('Frequency (Hz)')
+    # plt.ylabel('Combined Amplitude')
+    # plt.title('Combined Frequency Spectrum')
+    # plt.legend()
 
     # Adjust layout
     plt.tight_layout()

scan_pattern_tool.py
@@ -6,35 +6,21 @@ from matplotlib.animation import FuncAnimation
 import argparse
 import scipy.optimize # for sin fit
 
-'''
-
-
-'''
 ''' Animate the x,y,z rows of a data frame '''
 def animate_xyz_df(data, skip_points,nrows=20000):
     # Read the CSV file
-    data = data
+    data = pd.read_csv(data)
     pd.options.display.max_columns = None
     # print(data.head())
 
-    # print(only_points_coord.head())
     # print(only_points_coord.min())
     # print(only_points_coord.max())
 
     # Extract columns
-    if 'Time/s' in data.columns:
-        time = data['Time/s']
-        azimuth = data['Azimuth/deg']
-        zenith = data['Zenith/deg']
-    elif 'x' in data.columns:
-        time = data['x']
-        azimuth = data['y']
-        zenith = data['z']
-    elif 'X'  in data.columns:
-        time = data['X']
-        azimuth = data['Y']
-        zenith = data['Z']
-        
+
+    time = data['x']
+    azimuth = data['y']
+    zenith = data['z']
 
     # Function to update the plot with skipped points
     def update_plot(skip_points):
@@ -83,10 +69,10 @@ def animate_xyz_df(data, skip_points,nrows=20000):
 ''' Animate the zenith and azimuth rows of a data frame '''
 def animate_polar_df(data, skip_points,nrows=20000):
     # Read the CSV file
-    data = data
+    data = pd.read_csv(data,nrows=nrows)
     azimuth = data['azimuth']
     zenith = data['zenith']
-    time = data['index']
+    index = data['index']
 
     #convert to cartesian (R=1 for normalization)
     r = 30
@@ -105,6 +91,7 @@ def animate_polar_df(data, skip_points,nrows=20000):
         x_skipped = x[::skip_points]
         y_skipped = y[::skip_points]
         z_skipped = z[::skip_points]
+        index_skipped = index[::skip_points]
 
         # Set up the figure and 3D axis
         fig = plt.figure()
@@ -126,16 +113,18 @@ def animate_polar_df(data, skip_points,nrows=20000):
 
         # Initialize the point cloud
         point_cloud, = ax.plot([], [], [], 'ro',markersize=4)
+        
 
         # Update function for animation
         def update(frame):
             # Update the point cloud data
             point_cloud.set_data( x_skipped[:frame+1], y_skipped[:frame+1])
             point_cloud.set_3d_properties(z_skipped[:frame+1])
+            print(index[index_skipped[:frame+1]])
             return point_cloud,
 
         # Create the animation
-        ani = FuncAnimation(fig, update, frames=len(x_skipped), interval=10, blit=True)
+        ani = FuncAnimation(fig, update, frames=len(x_skipped), interval=1, blit=True)
         # Show the plot
         plt.show()
 
@@ -254,11 +243,12 @@ if __name__ == '__main__':
     #data = pd.read_csv('livox_indoor_1_scans_with_polar.csv', delimiter=',', nrows=20000)
     # data = converted_data
     # animate_xyz_df(data, args.skip_points)
-    data = pd.read_csv('/home/sqdr/ROSDOCKER/noetic/src/point_lio_ws/data_filtering/data/output_data.csv',delimiter=',',nrows=2100)
-    animate_polar_df(data, args.skip_points)
+    # data = pd.read_csv('/home/sqdr/ROSDOCKER/noetic/src/point_lio_ws/data_filtering/data/output_data.csv',delimiter=',',nrows=2100)
+    # animate_polar_df(data, args.skip_points)
 
     #Plot scan patter in polar
     '''for know I have to add the 'index' as the first column's name'''
     # data = pd.read_csv(args.file_path, delimiter=',', nrows=args.nrows)
-    plot_polar_scan_patter(data)
+    #plot_polar_scan_patter()
+    animate_polar_df(args.file_path, args.skip_points, args.nrows)
 

test_scripts/fov_2_average_gap.py
@@ -1,42 +1,90 @@
-import csv
 import pandas as pd
 import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 def compute_average_gaps(fov_array):
-
+    
     def compute_line_gaps(array):
-        total_sum_of_averages = 0
-        total_lines = 0
+        # Initialize the list to store line average gaps
+        line_average_gaps = []
         
-        for line in array:
+        num_rows, num_cols = array.shape
+        
+        def get_valid_range(index, length):
+            start = max(0, index - 4)
+            end = min(length, index + 4)  # index + 2 is exclusive
+            return start, end
+        
+        def compute_gap(last_non_zero_pos, index, extended_line, start, end):
+            min_hor_dist = float('inf')
+            min_vert_dist = float('inf')
+
+            if extended_line.ndim == 2:
+                for row_index, col_index in np.ndindex(extended_line.shape):
+                    element = extended_line[row_index, col_index]
+                    if element != 0:
+                        dist_vert = abs(row_index - (index - start))  # Vertical distance (row difference)
+                        dist_hor = abs(col_index - index)  # Horizontal distance (column difference)
+
+                        # Only consider horizontal distance if it is larger than vertical distance
+                        if dist_hor > dist_vert and dist_hor < min_hor_dist:
+                            min_hor_dist = dist_hor
+                            min_vert_dist = dist_vert  # This is just informative but not used in the final result
+
+            elif extended_line.ndim == 1:
+                for i, value in enumerate(extended_line):
+                    if value != 0:
+                        current_hor_dist = abs(i - last_non_zero_pos)
+                        if current_hor_dist < min_hor_dist:
+                            min_hor_dist = current_hor_dist
+
+            return min_hor_dist
+        
+        def process_line(line, extended_line, start, end):
             last_non_zero_pos = None
             line_sum_of_gaps = 0
             count_non_zero_elements = 0
-
+            
             for index, value in enumerate(line):
                 if value != 0:
                     if last_non_zero_pos is not None:
-                        gap = index - last_non_zero_pos
+                        gap = compute_gap(last_non_zero_pos, index, extended_line, start, end)
                         line_sum_of_gaps += gap
                         count_non_zero_elements += 1
-
                     last_non_zero_pos = index
-
-            if count_non_zero_elements > 1:
+            
+            if count_non_zero_elements > 4:
                 line_average_gap = line_sum_of_gaps / (count_non_zero_elements - 1)
-                total_sum_of_averages += line_average_gap
-                total_lines += 1
-
-        return total_sum_of_averages / total_lines if total_lines > 0 else None
+                line_average_gaps.append(line_average_gap)
+        
+        for i in range(num_rows):
+            # Get valid range of lines to consider
+            start_row, end_row = get_valid_range(i, num_rows)
+            # Collect data from the lines within the range
+            if end_row - start_row > 1:
+                extended_line = np.concatenate(array[start_row:end_row], axis=0)
+            else:
+                extended_line = array[start_row:end_row][0]
+            line = array[i]
+            process_line(line, extended_line, start_row, end_row)
+        
+        # Calculate and return the average of all line average gaps
+        if line_average_gaps:
+            overall_average_gap = sum(line_average_gaps) / len(line_average_gaps)
+        else:
+            overall_average_gap = None
+        
+        return overall_average_gap, line_average_gaps
 
     # Horizontal gaps
-    horizontal_average_gap = compute_line_gaps(fov_array)
+    horizontal_average_gap, horizontal_gaps = compute_line_gaps(fov_array)
+    print(f"Horizontal average gap = {horizontal_average_gap:.2f}")
 
     # Vertical gaps
-    vertical_average_gap = compute_line_gaps(fov_array.T)
-
+    vertical_average_gap, vertical_gaps = compute_line_gaps(fov_array.T)
+    print(f"Vertical average gap = {vertical_average_gap:.2f}")
 
     if horizontal_average_gap is not None:
         print(f"The final horizontal average gap is: {horizontal_average_gap:.2f} pixels ")
@@ -50,12 +98,25 @@ def compute_average_gaps(fov_array):
     else:
         print("No valid data to compute vertical average gap.")
 
+    # Save gaps to CSV
+    with open('gaps.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Direction', 'Line Index', 'Gap'])
+        
+        # Write horizontal gaps
+        for index, gap in enumerate(horizontal_gaps):
+            writer.writerow(['Horizontal', index, gap])
+        
+        # Write vertical gaps
+        for index, gap in enumerate(vertical_gaps):
+            writer.writerow(['Vertical', index, gap])
+
     return horizontal_average_gap, vertical_average_gap, (360/314)*horizontal_average_gap, (88/314)*vertical_average_gap
 
-def call_command(number_scans):
+def calculate_fov_heatmap(number_scans,file):
     # Define the command and its arguments
     number_scans = number_scans
-    command = ["python3", "/home/sqdr/ROSDOCKER/noetic/src/point_lio_ws/data_filtering/test_scripts/fov_heatmap.py", "/home/sqdr/ROSDOCKER/noetic/src/point_lio_ws/data_filtering/data/output_data.csv", str(number_scans),"--no_plot"]
+    command = ["python3", "/home/sqdr/ROSDOCKER/noetic/src/data_lidar/data_filtering_scripts/fov_heatmap.py", str(file), str(number_scans),"--no_plot"] # ,"--no_plot"
 
     # Run the command
     try:
@@ -63,33 +124,37 @@ def call_command(number_scans):
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         
         # Print the standard output and error
-        print("Standard Output:", result.stdout)
-        print("Standard Error:", result.stderr)
-        print("Return Code:", result.returncode)
+        # print("Standard Output:", result.stdout)
+        # print("Standard Error:", result.stderr)
+        # print("Return Code:", result.returncode)
     
     except subprocess.CalledProcessError as e:
         print("An error occurred while executing the command.")
         print("Error Output:", e.stderr)
         print("Return Code:", e.returncode)
 
-# Example usage
-filename = '/home/sqdr/ROSDOCKER/noetic/src/point_lio_ws/data_filtering/data/heatmap/cropped_fov_2D_array.csv'
+file_to_analyse = "/home/sqdr/ROSDOCKER/noetic/src/data_lidar/ROSBAG/SITE_referece_measurements/output_data.csv"
+fov_filename = '/home/sqdr/ROSDOCKER/noetic/src/data_lidar/data_filtering_scripts/test_scripts/fov_2D_array.csv'
 horizontal_progression = []
 vertical_progression = []
-scan_number = 60
+scan_number = 15
 
-for item in range(1,scan_number):
-    call_command(item)
-    df = pd.read_csv(filename,delimiter=',')
+for number in range(1,scan_number):
+    # Save the field of view of the the number on a file fov_2D_array.csv
+    calculate_fov_heatmap(number,file_to_analyse)
+
+    # Read this file
+    df = pd.read_csv(fov_filename,delimiter=',')
     fov_array = df.to_numpy()
 
+    # Takes the fov array and compute the average gaps against a certain criterium
     _, _, horizontal_result, vertical_result = compute_average_gaps(fov_array)
     horizontal_progression.append(horizontal_result)
     vertical_progression.append(vertical_result)
 
 print(horizontal_result)
 # Correct:
-x = np.linspace(0.1,(scan_number-1)/10,scan_number) 
+x = np.linspace(0.1,(scan_number)/10,scan_number-1) 
 print(x)
 plt.plot(x, horizontal_progression, 'r--', linewidth=3, label='horizontal')
 plt.plot(x, vertical_progression, 'b--', linewidth=3, label='vertical')